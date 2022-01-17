063482fae27355ff4efb0c033d48925c639d357e
Lviat Yi <LviatYi@qq.com>
2022-01-18 00:53:34 +0800

    Add Noise Tools Kit

---
+++ Assets/Scripts/NoiseKit.cs
@@ -0,0 +1,7 @@
+﻿namespace Kit {
+    public static class NoiseKit {
+        public static float PerlinNoise(float x, float y, float seed, float frequency = 3, float amplitude = 100) {
+            return UnityEngine.Mathf.PerlinNoise((x + seed) / frequency, (y + seed) / frequency) * amplitude;
+        }
+    }
+}
