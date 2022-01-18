f8ec76c1679fcbed6b101b950f65a949da53f247
Lviat Yi <LviatYi@qq.com>
2022-01-18 11:19:34 +0800

    Create ViewController

---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ViewController : MonoBehaviour {
+    [Header("Player attr")]
+    [SerializeField]
+    private int sight;
+
+    [SerializeField]
+    private Tilemap map;
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
