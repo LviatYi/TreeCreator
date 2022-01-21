9489a358c8ecb83ce3c0de3379d18e29fc025a71
Lviat Yi <LviatYi@qq.com>
2022-01-21 12:45:06 +0800

    Create Main Controller

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 主 Controller 类。
+/// </summary>
+/// <author>LviatYi</author>
+public class MainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
